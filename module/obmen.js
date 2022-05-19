2fa71f7e79e0d8abee0e6b6967a3dd9643ae748a
dim4ego <sitezico@gmail.com>
2022-05-19 14:37:49 +0300

    добавился обмен

--- module/obmen.js
+++ module/obmen.js
@@ -5,38 +5,82 @@ let file1c = "C:\\Program Files\\1cv8\\8.3.20.1674\\bin\\1cv8.exe "
 let arg_disstames="/DisableStartupMessages "
 let arg_type="enterprise "
 let path_script ="d:\\Scripts\\exchange\\1s8\\buhsh\\"
+let prefixForSQLDB ="/S"
+let prefixForFileDb = "/F"
+let prefixForLogin = "/N"
+let prefixForPass = "/P"
+let prefixForEpf = "/Execute"
+let prefixForLog = "/OUT"
+
+console.log(choseBase("ud", "up"))
+
+function choseBase(base, upOrDown){
+    if (upOrDown == "upload" & base ==) {
+        
+    }
+}
+    
 
 
 
-let holding = {
+let cn = {
     upload:  {
         
-        login1c:"Exchange_UD_OUT ",
-        db:"D:\\DB1S8\\Holding\\BuhSh\\ ",
+        login1c:prefixForLogin+"Exchange_UD_OUT ",
+        key:"cn_ud",
+        db:prefixForFileDb+"D:\\DB1S8\\Holding\\BuhSh\\ ",
         pathExchange:"D:\\DB1S8\\Holding\\BuhSh\\exchange\\ ",
-        password_1s:"Fag90TMng ",
-        arg_log : path_script+ "ud\\log\\ ",
-        epf_exit :"D:\\Scripts\\Exchange\\1S8\\BuhSh\\ud\\bin\\exit.epf ",
+        password_1s:prefixForPass+"Fag90TMng ",
+        arg_log :prefixForLog+path_script+ "ud\\log\\ ",
+        epf_exit :prefixForEpf+"D:\\Scripts\\Exchange\\1S8\\BuhSh\\ud\\bin\\exit.epf ",
         ftp_password:"Hsg45^weR ",
         ftp_user : 'Exchange1S8BuhSh_ud '
 
     },
 
     download: {
-        login1c : "Exchange_UD_IN ",
-        db:"D:\\DB1S8\\Holding\\BuhSh\\ ",
+        login1c :prefixForLogin+ "Exchange_UD_IN ",
+        key:"ud_cn"
+        db:prefixForFileDb+"D:\\DB1S8\\Holding\\BuhSh\\ ",
         pathExchange:"D:\\DB1S8\\Holding\\BuhSh\\exchange\\ ",
-        password_1s:"Fag90TMng ",
-        arg_log : path_script+ "ud\\log\\ ",
-        epf_exit :"D:\\Scripts\\Exchange\\1S8\\BuhSh\\ud\\bin\\exit.epf ",
+        password_1s:prefixForPass+"Fag90TMng ",
+        arg_log :prefixForLog+path_script+ "ud\\log\\ ",
+        epf_exit :prefixForEpf+"D:\\Scripts\\Exchange\\1S8\\BuhSh\\ud\\bin\\exit.epf ",
+        ftp_password:"Hsg45^weR ",
+        ftp_user : 'Exchange1S8BuhSh_ud '
+    }     
+
+}
+
+
+
+
+let holding = {
+    upload:  {
+        
+        login1c:prefixForLogin+"Exchange_UD_OUT ",
+        key:"cn_ud",
+        db:prefixForFileDb+"D:\\DB1S8\\Holding\\BuhSh\\ ",
+        pathExchange:"D:\\DB1S8\\Holding\\BuhSh\\exchange\\ ",
+        password_1s:prefixForPass+"Fag90TMng ",
+        arg_log :prefixForLog+path_script+ "ud\\log\\ ",
+        epf_exit :prefixForEpf+"D:\\Scripts\\Exchange\\1S8\\BuhSh\\ud\\bin\\exit.epf ",
         ftp_password:"Hsg45^weR ",
         ftp_user : 'Exchange1S8BuhSh_ud '
 
+    },
 
-
-    }
-    
-    
+    download: {
+        login1c :prefixForLogin+ "Exchange_UD_IN ",
+        key:"ud_cn"
+        db:prefixForFileDb+"D:\\DB1S8\\Holding\\BuhSh\\ ",
+        pathExchange:"D:\\DB1S8\\Holding\\BuhSh\\exchange\\ ",
+        password_1s:prefixForPass+"Fag90TMng ",
+        arg_log :prefixForLog+path_script+ "ud\\log\\ ",
+        epf_exit :prefixForEpf+"D:\\Scripts\\Exchange\\1S8\\BuhSh\\ud\\bin\\exit.epf ",
+        ftp_password:"Hsg45^weR ",
+        ftp_user : 'Exchange1S8BuhSh_ud '
+    }     
 
 }
 
@@ -71,24 +115,14 @@ class Exchange {
 
     }
     download(){
-
-        
+        let zapros = this.file1c+this.arg_disstames+this.arg_type+this.db+this.login1c+this.password_1s+this.epf_exit+this.arg_log
+        console.log(zapros)
     }
-
-
-
-
-
-
-
-
-
-
-
-
-
 
 }
 
-let azm = new Exchange(file1c,arg_disstames,arg_type,holding.upload.db,holding.upload.login1c,holding.upload.password_1s,holding.upload.epf_exit,holding.upload.arg_log);
-azm.upload()
+let holdingUpload = new Exchange(file1c,arg_disstames,arg_type,holding.upload.db,holding.upload.login1c,holding.upload.password_1s,holding.upload.epf_exit,holding.upload.arg_log);
+let holdingDownload = new Exchange(file1c,arg_disstames,arg_type,holding.upload.db,holding.download.login1c,holding.upload.password_1s,holding.upload.epf_exit,holding.upload.arg_log);
+
+holdingDownload.download()
+holdingUpload.upload()
